0bb63403d1fdf9375e9db6bb562293ad94232fd7
rsdbkhusky <78398573+rsdbkhusky@users.noreply.github.com>
2022-04-03 20:38:52 +0800

    Add zhangzhiqi

--- RevolutionBP.user.js
+++ RevolutionBP.user.js
@@ -46,22 +46,28 @@
     $('.uoj-honor').each(function(){$(this).text().includes('2021xiefengyu')&&$(this).html('Ignotus')})
     $('.uoj-username').each(function(){$(this).text().includes('Ignotus') && $(this).css('color', '#a938f1')})
     $('.uoj-honor').each(function(){$(this).text().includes('Ignotus') && $(this).css('color', '#a938f1')})
-    
+
     $('.uoj-username').each(function(){$(this).text().includes('Ignotus')&&$(this).css('color','#a938f1')});
     $('.uoj-honor').each(function(){$(this).text().includes('Ignotus')&&$(this).css('color','#a938f1')});
-    
+
     $('.uoj-username').each(function(){$(this).text().includes('2021guoyixiao')&&$(this).html('Fideow')})
     $('.uoj-honor').each(function(){$(this).text().includes('2021guoyixiao')&&$(this).html('Fideow')})
 
     $('.uoj-username').each(function(){$(this).text().includes('2021liuzipei')&&$(this).html('卡哇伊沛沛')})
     $('.uoj-honor').each(function(){$(this).text().includes('2021liuzipei')&&$(this).html('卡哇伊沛沛')})
 
-     $('.uoj-username').each(function(){$(this).text().includes('2020luorongxin')&&$(this).css('color', 'rgb(96,181,244)') })
+    $('.uoj-username').each(function(){$(this).text().includes('2020luorongxin')&&$(this).css('color', 'rgb(96,181,244)') })
     $('.uoj-username').each(function(){$(this).text().includes('2020luorongxin (罗荣鑫)')&&$(this).html('X</span><span class=\"uoj-username\" style=\"color:rgb(80,80,80)\">iEn1847 (罗荣鑫)'); })
     $('.uoj-username').each(function(){$(this).text().includes('2020luorongxin')&&$(this).html('X</span><span class=\"uoj-username\" style=\"color:rgb(80,80,80)\">iEn1847'); })
     $('.uoj-honor').each(function(){$(this).text().includes('2020luorongxin')&&$(this).css('color', 'rgb(96,181,244)') })
     $('.uoj-honor').each(function(){$(this).text().includes('2020luorongxin (罗荣鑫)')&&$(this).html('X</span><span class=\"uoj-username\" style=\"color:rgb(80,80,80)\">iEn1847 (罗荣鑫)'); })
     $('.uoj-honor').each(function(){$(this).text().includes('2020luorongxin')&&$(this).html('X</span><span class=\"uoj-username\" style=\"color:rgb(80,80,80)\">iEn1847'); })
 
+    $('.uoj-username').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).css('color', 'rgb(0,0,0)') })
+    $('.uoj-username').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>'); })
+    $('.uoj-username').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>'); })
+    $('.uoj-honor').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).css('color', 'rgb(0,0,0)') })
+    $('.uoj-honor').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>'); })
+    $('.uoj-honor').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>'); })
     // Your code here...
 })();
