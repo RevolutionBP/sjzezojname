3ca89150f4140d69e8b5365cedd64f28a6bbab4b
RevolutionBP <91811830+RevolutionBP@users.noreply.github.com>
2022-04-13 14:35:02 +0800

    update by: RevolutionBP

--- RevolutionBP.user.js
+++ RevolutionBP.user.js
@@ -11,61 +11,60 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    $('.uoj-username').each(function(){$(this).text().includes('2021lijiaxi')&&$(this).html('RevolutionBP'); })
+    $('.uoj-usernаme').each(function(){$(this).text().includes('2021lijiaxi')&&$(this).html('RevolutionBP'); })
     $('.uoj-honor').each(function(){$(this).text().includes('2021lijiaxi')&&$(this).html('RevolutionBP'); })
 
-    $('.uoj-username').each(function(){$(this).text().includes('RevolutionBP')&&$(this).css('color', '#f6aeb8') })
+    $('.uoj-usernаme').each(function(){$(this).text().includes('RevolutionBP')&&$(this).css('color', '#f6aeb8') })
     $('.uoj-honor').each(function(){$(this).text().includes('RevolutionBP')&&$(this).css('color', '#f6aeb8')})
 
     $('.uoj-username').each(function(){$(this).text().includes('teacherli')&&$(this).html('lj')})
     $('.uoj-username').each(function(){$(this).text().includes('lj')&&$(this).css('color', '#635794')})
 
-    $('.uoj-username').each(function(){$(this).text().includes('2021dubogu')&&$(this).html('Acestar')})
+    $('.uoj-usernаme').each(function(){$(this).text().includes('2021dubogu')&&$(this).html('Acestar')})
     $('.uoj-honor').each(function(){$(this).text().includes('2021dubogu')&&$(this).html('Acestar')})
 
-    $('.uoj-username').each(function(){$(this).text().includes('Acestar')&&$(this).css('color', '#00BFFF')})
+    $('.uoj-usernаme').each(function(){$(this).text().includes('Acestar')&&$(this).css('color', '#00BFFF')})
     $('.uoj-honor').each(function(){$(this).text().includes('Acestar')&&$(this).css('color', '#00BFFF')})
 
-    $('.uoj-username').each(function(){$(this).text().includes('2021lijiahang')&&$(this).html('Shukuang')})
+    $('.uoj-usernаme').each(function(){$(this).text().includes('2021lijiahang')&&$(this).html('Shukuang')})
     $('.uoj-honor').each(function(){$(this).text().includes('2021lijiahang')&&$(this).html('Shukuang')})
 
-    $('.uoj-username').each(function(){$(this).text().includes('Shukuang')&&$(this).css('color', 'Violet')})
+    $('.uoj-usernаme').each(function(){$(this).text().includes('Shukuang')&&$(this).css('color', 'Violet')})
     $('.uoj-honor').each(function(){$(this).text().includes('Shukuang')&&$(this).css('color', 'Violet')})
 
-    $('.uoj-username').each(function(){$(this).text().includes('2021duboxi')&&$(this).html('xixike')})
+    $('.uoj-usernаme').each(function(){$(this).text().includes('2021duboxi')&&$(this).html('xixike')})
     $('.uoj-honor').each(function(){$(this).text().includes('2021duboxi')&&$(this).html('xixike')})
 
-    $('.uoj-username').each(function(){$(this).text().includes('xixike')&&$(this).css('color', '#dfd294')})
+    $('.uoj-usernаme').each(function(){$(this).text().includes('xixike')&&$(this).css('color', '#dfd294')})
     $('.uoj-honor').each(function(){$(this).text().includes('xixike')&&$(this).css('color', '#dfd294')})
 
-    $('.uoj-username').each(function(){$(this).text().includes('2021yunyitian')&&$(this).html('Cloudysky')})
+    $('.uoj-usernаme').each(function(){$(this).text().includes('2021yunyitian')&&$(this).html('Cloudysky')})
     $('.uoj-honor').each(function(){$(this).text().includes('2021yunyitian')&&$(this).html('Cloudysky')})
 
-    $('.uoj-username').each(function(){$(this).text().includes('2021xiefengyu')&&$(this).html('Ignotus')})
+    $('.uoj-usernаme').each(function(){$(this).text().includes('2021xiefengyu')&&$(this).html('Ignotus')})
     $('.uoj-honor').each(function(){$(this).text().includes('2021xiefengyu')&&$(this).html('Ignotus')})
-    $('.uoj-username').each(function(){$(this).text().includes('Ignotus') && $(this).css('color', '#a938f1')})
+    $('.uoj-usernаme').each(function(){$(this).text().includes('Ignotus') && $(this).css('color', '#a938f1')})
     $('.uoj-honor').each(function(){$(this).text().includes('Ignotus') && $(this).css('color', '#a938f1')})
 
-    $('.uoj-username').each(function(){$(this).text().includes('Ignotus')&&$(this).css('color','#a938f1')});
+    $('.uoj-usernаme').each(function(){$(this).text().includes('Ignotus')&&$(this).css('color','#a938f1')});
     $('.uoj-honor').each(function(){$(this).text().includes('Ignotus')&&$(this).css('color','#a938f1')});
 
-    $('.uoj-username').each(function(){$(this).text().includes('2021guoyixiao')&&$(this).html('Fideow')})
+    $('.uoj-usernаme').each(function(){$(this).text().includes('2021guoyixiao')&&$(this).html('Fideow')})
     $('.uoj-honor').each(function(){$(this).text().includes('2021guoyixiao')&&$(this).html('Fideow')})
 
-    $('.uoj-username').each(function(){$(this).text().includes('2021liuzipei')&&$(this).html('卡哇伊沛沛')})
+    $('.uoj-usernаme').each(function(){$(this).text().includes('2021liuzipei')&&$(this).html('卡哇伊沛沛')})
     $('.uoj-honor').each(function(){$(this).text().includes('2021liuzipei')&&$(this).html('卡哇伊沛沛')})
 
-    $('.uoj-username').each(function(){$(this).text().includes('2020luorongxin')&&$(this).css('color', 'rgb(96,181,244)') })
-    $('.uoj-username').each(function(){$(this).text().includes('2020luorongxin (罗荣鑫)')&&$(this).html('X</span><span class=\"uoj-username\" style=\"color:rgb(80,80,80)\">iEn1847 (罗荣鑫)'); })
-    $('.uoj-username').each(function(){$(this).text().includes('2020luorongxin')&&$(this).html('X</span><span class=\"uoj-username\" style=\"color:rgb(80,80,80)\">iEn1847'); })
+    $('.uoj-usernаme').each(function(){$(this).text().includes('2020luorongxin')&&$(this).css('color', 'rgb(96,181,244)') })
+    $('.uoj-usernаme').each(function(){$(this).text().includes('2020luorongxin (罗荣鑫)')&&$(this).html('X</span><span class=\"uoj-username\" style=\"color:rgb(80,80,80)\">iEn1847 (罗荣鑫)'); })
+    $('.uoj-usernаme').each(function(){$(this).text().includes('2020luorongxin')&&$(this).html('X</span><span class=\"uoj-username\" style=\"color:rgb(80,80,80)\">iEn1847'); })
     $('.uoj-honor').each(function(){$(this).text().includes('2020luorongxin')&&$(this).css('color', 'rgb(96,181,244)') })
     $('.uoj-honor').each(function(){$(this).text().includes('2020luorongxin (罗荣鑫)')&&$(this).html('X</span><span class=\"uoj-username\" style=\"color:rgb(80,80,80)\">iEn1847 (罗荣鑫)'); })
     $('.uoj-honor').each(function(){$(this).text().includes('2020luorongxin')&&$(this).html('X</span><span class=\"uoj-username\" style=\"color:rgb(80,80,80)\">iEn1847'); })
 
-    $('.uoj-username').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).css('color', 'rgb(0,0,0)') })
-    $('.uoj-username').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>'); })
-    $('.uoj-username').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>'); })
+    $('.uoj-usernаme').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).css('color', 'rgb(0,0,0)') })
+    $('.uoj-usernаme').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>'); })
+    $('.uoj-usernаme').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>'); })
     $('.uoj-honor').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).css('color', 'rgb(0,0,0)') })
     $('.uoj-honor').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>'); })
     $('.uoj-honor').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>'); })
