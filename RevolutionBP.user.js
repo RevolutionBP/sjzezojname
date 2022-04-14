6f97d9ff49a8242be1a999f79f221265dcf7a5b0
Xi'En <79648987+XiEn1847@users.noreply.github.com>
2022-04-14 21:17:23 +0800

    Fix More Bugs
    oof

--- RevolutionBP.user.js
+++ RevolutionBP.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         sjzezojname
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      1.0.3
 // @description  light up the sky!
 // @author       RevolutionBP
 // @match        https://www.sjzezoj.com/*
@@ -13,56 +13,56 @@
 (function() {
 	'use strict';
 
-	$("span:contains('2021lijiaxi')").html('RevolutionBP')
-	$("span:contains('RevolutionBP')").css('color', '#f6aeb8')
-	$("a:contains('2021lijiaxi')").html('RevolutionBP')
-	$("a:contains('RevolutionBP')").css('color', '#f6aeb8')
+	$("span[style]:contains('2021lijiaxi')").html('RevolutionBP')
+	$("span[style]:contains('RevolutionBP')").css('color', '#f6aeb8')
+	$("a[style]:contains('2021lijiaxi')").html('RevolutionBP')
+	$("a[style]:contains('RevolutionBP')").css('color', '#f6aeb8')
 
-	$("span:contains('teacherli')").html('lj')
-	$("span:contains('lj')").css('color', '#635794')
-	$("a:contains('teacherli')").html('lj')
-	$("a:contains('lj')").css('color', '#635794')
+	$("span[style]:contains('teacherli')").html('lj')
+	$("span[style]:contains('lj')").css('color', '#635794')
+	$("a[style]:contains('teacherli')").html('lj')
+	$("a[style]:contains('lj')").css('color', '#635794')
 
-	$("span:contains('2021dubogu')").html('Acestar')
-	$("span:contains('Acestar')").css('color', '#00BFFF')
-	$("a:contains('2021dubogu')").html('Acestar')
-	$("a:contains('Acestar')").css('color', '#00BFFF')
+	$("span[style]:contains('2021dubogu')").html('Acestar')
+	$("span[style]:contains('Acestar')").css('color', '#00BFFF')
+	$("a[style]:contains('2021dubogu')").html('Acestar')
+	$("a[style]:contains('Acestar')").css('color', '#00BFFF')
 
 
-	$("span:contains('2021lijiahang')").html('Shukuang')
-	$("span:contains('Shukuang')").css('color', 'Violet')
-	$("a:contains('2021lijiahang')").html('Shukuang')
-	$("a:contains('Shukuang')").css('color', 'Violet')
+	$("span[style]:contains('2021lijiahang')").html('Shukuang')
+	$("span[style]:contains('Shukuang')").css('color', 'Violet')
+	$("a[style]:contains('2021lijiahang')").html('Shukuang')
+	$("a[style]:contains('Shukuang')").css('color', 'Violet')
 
-	$("span:contains('2021duboxi')").html('xixike')
-	$("span:contains('xixike')").css('color', '#dfd294')
-	$("a:contains('2021duboxi')").html('xixike')
-	$("a:contains('xixike')").css('color', '#dfd294')
+	$("span[style]:contains('2021duboxi')").html('xixike')
+	$("span[style]:contains('xixike')").css('color', '#dfd294')
+	$("a[style]:contains('2021duboxi')").html('xixike')
+	$("a[style]:contains('xixike')").css('color', '#dfd294')
 
-	$("span:contains('2021yunyitian')").html('Cloudysky')
-	$("a:contains('2021yunyitian')").html('Cloudysky')
+	$("span[style]:contains('2021yunyitian')").html('Cloudysky')
+	$("a[style]:contains('2021yunyitian')").html('Cloudysky')
 
-	$("span:contains('2021xiefengyu')").html('Ignotus')
-	$("span:contains('Ignotus')").css('color', '#a938f1')
-	$("a:contains('2021xiefengyu')").html('Ignotus')
-	$("a:contains('Ignotus')").css('color', '#a938f1')
+	$("span[style]:contains('2021xiefengyu')").html('Ignotus')
+	$("span[style]:contains('Ignotus')").css('color', '#a938f1')
+	$("a[style]:contains('2021xiefengyu')").html('Ignotus')
+	$("a[style]:contains('Ignotus')").css('color', '#a938f1')
 
-	$("span:contains('2021guoyixiao')").html('Fideow')
-	$("a:contains('2021guoyixiao')").html('Fideow')
+	$("span[style]:contains('2021guoyixiao')").html('Fideow')
+	$("a[style]:contains('2021guoyixiao')").html('Fideow')
 
-	$("span:contains('2021liuzipei')").html('卡哇伊沛沛')
-	$("a:contains('2021liuzipei')").html('卡哇伊沛沛')
+	$("span[style]:contains('2021liuzipei')").html('卡哇伊沛沛')
+	$("a[style]:contains('2021liuzipei')").html('卡哇伊沛沛')
 
-	$("span:contains('2020luorongxin')").css('color', 'rgb(96,181,244)')
-	$("span:contains('2020luorongxin')").html('X</span><span style=\"color:rgb(80,80,80)\">iEn1847')
-	$("a:contains('2020luorongxin')").css('color', 'rgb(96,181,244)')
-	$("a:contains('2020luorongxin')").html('X</span><span style=\"color:rgb(80,80,80)\">iEn1847')
+	$("span[style]:contains('2020luorongxin')").css('color', 'rgb(96,181,244)')
+	$("span[style]:contains('2020luorongxin')").html('X</span><span style=\"color:rgb(80,80,80)\">iEn1847')
+	$("a[style]:contains('2020luorongxin')").css('color', 'rgb(96,181,244)')
+	$("a[style]:contains('2020luorongxin')").html('X</span><span style=\"color:rgb(80,80,80)\">iEn1847')
 
-	$("span:contains('zhangzhiqi')").css('color', 'rgb(0,0,0)')
-	$("span:contains('zhangzhiqi')").html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>')
-	$("span:contains('zhangzhiqi')").html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>')
-	$("a:contains('zhangzhiqi')").css('color', 'rgb(0,0,0)')
-	$("a:contains('zhangzhiqi')").html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>')
-	$("a:contains('zhangzhiqi')").html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>')
+	$("span[style]:contains('zhangzhiqi')").css('color', 'rgb(0,0,0)')
+	$("span[style]:contains('zhangzhiqi')").html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>')
+	$("span[style]:contains('zhangzhiqi')").html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>')
+	$("a[style]:contains('zhangzhiqi')").css('color', 'rgb(0,0,0)')
+	$("a[style]:contains('zhangzhiqi')").html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>')
+	$("a[style]:contains('zhangzhiqi')").html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>')
 	// Your code here...
 })();
