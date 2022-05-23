78e322c2a63cdadaa78daf2692c6009df301bd83
RevolutionBP <91811830+RevolutionBP@users.noreply.github.com>
2022-05-23 19:13:38 +0800

    感谢longlong的二次开源

--- RevolutionBP.user.js
+++ RevolutionBP.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         RevolutionBP
+// @name         sjzezojname
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.4
 // @description  light up the sky!
-// @author       RevolutionBP
+// @author       RevolutionBP & XiEn1847
 // @match        https://www.sjzezoj.com/*
 // @match        https://sjzezoj.com/*
 // @icon         https://ftp.bmp.ovh/imgs/2021/12/81ce0eb68e885de0.jpg
@@ -11,62 +11,61 @@
 // ==/UserScript==
 
 (function() {
-    $('.uoj-usernаme').each(function(){$(this).text().includes('2021lijiaxi')&&$(this).html('RevolutionBP'); })
-    $('.uoj-honor').each(function(){$(this).text().includes('2021lijiaxi')&&$(this).html('RevolutionBP'); })
+	'use strict';
+//  usage: use $("span[style=\"font-weight: 800; color:#007bff\"]:contains('MYNAME')") to find your span
+//  then do .html('NEWNAME') to change name
+//  then do .css('color', '#NEWCOLOR') to change color   **(recommended to change name first cuz U'll have to find the span by its css)
 
-    $('.uoj-usernаme').each(function(){$(this).text().includes('RevolutionBP')&&$(this).css('color', '#f6aeb8') })
-    $('.uoj-honor').each(function(){$(this).text().includes('RevolutionBP')&&$(this).css('color', '#f6aeb8')})
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('2021lijiaxi')").html('RevolutionBP')
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('RevolutionBP')").css('color', '#f6aeb8')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('2021lijiaxi')").html('RevolutionBP')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('RevolutionBP')").css('color', '#f6aeb8')
 
-    $('.uoj-username').each(function(){$(this).text().includes('teacherli')&&$(this).html('lj')})
-    $('.uoj-username').each(function(){$(this).text().includes('lj')&&$(this).css('color', '#635794')})
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('teacherli')").html('lj')
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('lj')").css('color', '#635794')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('teacherli')").html('lj')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('lj')").css('color', '#635794')
 
-    $('.uoj-usernаme').each(function(){$(this).text().includes('2021dubogu')&&$(this).html('Acestar')})
-    $('.uoj-honor').each(function(){$(this).text().includes('2021dubogu')&&$(this).html('Acestar')})
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('2021dubogu')").html('Acestar')
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('Acestar')").css('color', '#00BFFF')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('2021dubogu')").html('Acestar')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('Acestar')").css('color', '#00BFFF')
 
-    $('.uoj-usernаme').each(function(){$(this).text().includes('Acestar')&&$(this).css('color', '#00BFFF')})
-    $('.uoj-honor').each(function(){$(this).text().includes('Acestar')&&$(this).css('color', '#00BFFF')})
 
-    $('.uoj-usernаme').each(function(){$(this).text().includes('2021lijiahang')&&$(this).html('Shukuang')})
-    $('.uoj-honor').each(function(){$(this).text().includes('2021lijiahang')&&$(this).html('Shukuang')})
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('2021lijiahang')").html('Shukuang')
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('Shukuang')").css('color', 'Violet')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('2021lijiahang')").html('Shukuang')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('Shukuang')").css('color', 'Violet')
 
-    $('.uoj-usernаme').each(function(){$(this).text().includes('Shukuang')&&$(this).css('color', 'Violet')})
-    $('.uoj-honor').each(function(){$(this).text().includes('Shukuang')&&$(this).css('color', 'Violet')})
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('2021duboxi')").html('xixike')
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('xixike')").css('color', '#dfd294')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('2021duboxi')").html('xixike')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('xixike')").css('color', '#dfd294')
 
-    $('.uoj-usernаme').each(function(){$(this).text().includes('2021duboxi')&&$(this).html('xixike')})
-    $('.uoj-honor').each(function(){$(this).text().includes('2021duboxi')&&$(this).html('xixike')})
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('2021yunyitian')").html('Cloudysky')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('2021yunyitian')").html('Cloudysky')
 
-    $('.uoj-usernаme').each(function(){$(this).text().includes('xixike')&&$(this).css('color', '#dfd294')})
-    $('.uoj-honor').each(function(){$(this).text().includes('xixike')&&$(this).css('color', '#dfd294')})
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('2021xiefengyu')").html('Ignotus')
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('Ignotus')").css('color', '#a938f1')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('2021xiefengyu')").html('Ignotus')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('Ignotus')").css('color', '#a938f1')
 
-    $('.uoj-usernаme').each(function(){$(this).text().includes('2021yunyitian')&&$(this).html('Cloudysky')})
-    $('.uoj-honor').each(function(){$(this).text().includes('2021yunyitian')&&$(this).html('Cloudysky')})
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('2021guoyixiao')").html('Fideow')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('2021guoyixiao')").html('Fideow')
 
-    $('.uoj-usernаme').each(function(){$(this).text().includes('2021xiefengyu')&&$(this).html('Ignotus')})
-    $('.uoj-honor').each(function(){$(this).text().includes('2021xiefengyu')&&$(this).html('Ignotus')})
-    $('.uoj-usernаme').each(function(){$(this).text().includes('Ignotus') && $(this).css('color', '#a938f1')})
-    $('.uoj-honor').each(function(){$(this).text().includes('Ignotus') && $(this).css('color', '#a938f1')})
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('2021liuzipei')").html('卡哇伊沛沛')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('2021liuzipei')").html('卡哇伊沛沛')
 
-    $('.uoj-usernаme').each(function(){$(this).text().includes('Ignotus')&&$(this).css('color','#a938f1')});
-    $('.uoj-honor').each(function(){$(this).text().includes('Ignotus')&&$(this).css('color','#a938f1')});
 
-    $('.uoj-usernаme').each(function(){$(this).text().includes('2021guoyixiao')&&$(this).html('Fideow')})
-    $('.uoj-honor').each(function(){$(this).text().includes('2021guoyixiao')&&$(this).html('Fideow')})
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('2020luorongxin')").html('</span><span style=\"color:rgb(96,181,244)\">X</span><span style=\"color:rgb(80,80,80)\">iEn1847</span><span>')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('2020luorongxin')").html('</span><span style=\"color:rgb(96,181,244)\">X</span><span style=\"color:rgb(80,80,80)\">iEn1847</span><span>')
 
-    $('.uoj-usernаme').each(function(){$(this).text().includes('2021liuzipei')&&$(this).html('卡哇伊沛沛')})
-    $('.uoj-honor').each(function(){$(this).text().includes('2021liuzipei')&&$(this).html('卡哇伊沛沛')})
 
-    $('.uoj-usernаme').each(function(){$(this).text().includes('2020luorongxin')&&$(this).css('color', 'rgb(96,181,244)') })
-    $('.uoj-usernаme').each(function(){$(this).text().includes('2020luorongxin (罗荣鑫)')&&$(this).html('X</span><span class=\"uoj-username\" style=\"color:rgb(80,80,80)\">iEn1847 (罗荣鑫)'); })
-    $('.uoj-usernаme').each(function(){$(this).text().includes('2020luorongxin')&&$(this).html('X</span><span class=\"uoj-username\" style=\"color:rgb(80,80,80)\">iEn1847'); })
-    $('.uoj-honor').each(function(){$(this).text().includes('2020luorongxin')&&$(this).css('color', 'rgb(96,181,244)') })
-    $('.uoj-honor').each(function(){$(this).text().includes('2020luorongxin (罗荣鑫)')&&$(this).html('X</span><span class=\"uoj-username\" style=\"color:rgb(80,80,80)\">iEn1847 (罗荣鑫)'); })
-    $('.uoj-honor').each(function(){$(this).text().includes('2020luorongxin')&&$(this).html('X</span><span class=\"uoj-username\" style=\"color:rgb(80,80,80)\">iEn1847'); })
-
-    $('.uoj-usernаme').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).css('color', 'rgb(0,0,0)') })
-    $('.uoj-usernаme').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>'); })
-    $('.uoj-usernаme').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>'); })
-    $('.uoj-honor').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).css('color', 'rgb(0,0,0)') })
-    $('.uoj-honor').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>'); })
-    $('.uoj-honor').each(function(){$(this).text().includes('zhangzhiqi')&&$(this).html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>'); })
-    // Your code here...
+//	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('zhangzhiqi')").css('color', 'rgb(0,0,0)')
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('zhangzhiqi')").html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>')
+	$("span[style=\"font-weight: 800; color:#007bff\"]:contains('zhangzhiqi')").html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>')
+//	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('zhangzhiqi')").css('color', 'rgb(0,0,0)')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('zhangzhiqi')").html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>')
+	$("a[style=\"font-weight: 800; color:#007bff\"]:contains('zhangzhiqi')").html('<span style="color: rgb(0,0,0);">r</span><span style="color: rgb(20,20,20);">s</span><span style="color: rgb(40,40,40);">d</span><span style="color: rgb(60,60,60);">b</span><span style="color: rgb(80,80,80);">k</span><span style="color: rgb(100,100,100);">_</span><span style="color: rgb(120,120,120);">h</span><span style="color: rgb(140,140,140);">u</span><span style="color: rgb(160,160,160);">s</span><span style="color: rgb(180,180,180);">k</span><span style="color: rgb(200,200,200);">y</span>')
+	// Your code here...
 })();
